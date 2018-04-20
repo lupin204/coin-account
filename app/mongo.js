2a99182976a841d187408180e5ddc831c740063a
Lee Young Seo <lupin204@gmail.com>
2018-04-20 11:08:11 +0900

    modify..

--- app/mongo.js
+++ app/mongo.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
-/*
-mongodb://lupin204:<dbpassword>@ds229458.mlab.com:29458/coinbalance
-*/
 //local
 const local_info = 'mongodb://localhost:27017/local';
 //mlab_유료
-const mlab_info = 'mongodb://lupin204:lupin204@ds121259-a0.mlab.com:21259/coinbalance';
-//mlab_무료
-//const mlab_info = 'mongodb://lupin204:lupin204@ds121099.mlab.com:21099/coinbalance_free';
+const mlab_info = process.env.MLAB_MONGO_INFO;
 
 
 module.exports = () => {
